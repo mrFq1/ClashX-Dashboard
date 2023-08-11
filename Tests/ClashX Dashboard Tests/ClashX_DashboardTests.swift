d53924b126ce2dfdd8c437c6d92ff82bf93d59db
mrFq1 <1xxbx0il0@mozmail.com>
2023-08-11 15:05:24 +0800

    msic: fix tests

--- Tests/ClashX Dashboard Tests/ClashX_DashboardTests.swift
+++ Tests/ClashX Dashboard Tests/ClashX_DashboardTests.swift
@@ -1,11 +1,11 @@
 import XCTest
-@testable import ClashX_Dashboard_Kit
+@testable import ClashX_Dashboard
 
-final class ClashX_Dashboard_KitTests: XCTestCase {
+final class ClashX_DashboardTests: XCTestCase {
     func testExample() throws {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct
         // results.
-////        XCTAssertEqual(ClashX_Dashboard_Kit().text, "Hello, World!")
+////        XCTAssertEqual(ClashX_Dashboard().text, "Hello, World!")
     }
 }
